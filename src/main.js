1392cd88e932f75cafd318f95587695c5250428b
sm-harun <harunyuss12@gmail.com>
2024-04-30 13:14:53 +0300

    Added vector display

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 class Plane {
-    constructor(context, canvasWidth, canvasHeight) {
+    constructor(context, canvasWidth, canvasHeight, divisions) {
         this.context = context;
         
         this.xOrigin = canvasWidth / 2;
@@ -7,6 +7,9 @@ class Plane {
         
         this.maxWidth = canvasWidth;
         this.maxHeight = canvasHeight;
+        
+        // This sets how much space one magnitude holds.
+        this.divisions = this.maxHeight/divisions;
     }
     
     drawPlane() {
@@ -22,11 +25,40 @@ class Plane {
         this.context.stroke();
         this.context.closePath();
     }
+    
+    drawVector(x, y, color) {
+        this.context.beginPath();
+        this.context.strokeStyle = color;
+        
+        this.context.moveTo(this.xOrigin, this.yOrigin);
+        this.context.lineTo(this.xOrigin + x*this.divisions, this.yOrigin - y*this.divisions);
+        
+        this.context.stroke();
+        this.context.closePath();
+    }
 }
 
 let canvas = document.getElementById("main-canvas");
 let cxt = canvas.getContext("2d");
 
-let plane = new Plane(cxt, canvas.width, canvas.height);
+let plane = new Plane(cxt, canvas.width, canvas.height, 20);
 
 plane.drawPlane();
+
+let firstVectorButton = document.getElementById("first-vector-button");
+let secondVectorButton = document.getElementById("second-vector-button");
+
+firstVectorButton.addEventListener("click", function() {
+    
+    let xCord = document.getElementById("first-vector-x").value;
+    let yCord = document.getElementById("first-vector-y").value;
+    
+    plane.drawVector(xCord, yCord, "red");
+});
+
+secondVectorButton.addEventListener("click", function() {
+    let xCord = document.getElementById("second-vector-x").value;
+    let yCord = document.getElementById("second-vector-y").value;
+    
+    plane.drawVector(xCord, yCord, "blue");
+});
